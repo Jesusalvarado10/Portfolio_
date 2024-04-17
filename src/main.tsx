d5ad0e1ff2a15925bd7b49b0cb61936ce667aa55
Jesus <97445036+Jesusalvarado10@users.noreply.github.com>
2024-04-17 01:53:05 -0400

    Fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+
 import './index.css'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { Home } from './Pages/Home/home.tsx'
